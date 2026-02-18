b4560c7e9d83692291926a0edaee07f56d6a736e
christina2748394 <138764863+christina2748394@users.noreply.github.com>
2026-02-17 17:22:12 -0800

    new

--- GDIM32 In-Class Activities/Assets/W7_PreLearning/Scripts/OriginalToTarget.cs
+++ GDIM32 In-Class Activities/Assets/W7_PreLearning/Scripts/OriginalToTarget.cs
@@ -5,9 +5,9 @@ public class OriginalToTarget : MonoBehaviour
     [SerializeField] private Transform _original;
     [SerializeField] private Transform _target;
 
-    private void Start ()
+    private void Start()
     {
-        // Vector3 displacementToTarget = ____ - ____;
-        // _original.position += displacementToTarget;
+        Vector3 displacementToTarget = _original - _target;
+        _original.position += displacementToTarget;
     }
 }
